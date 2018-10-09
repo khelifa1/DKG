557c2ed791f875849b27c3f05289d2fe1b1f8ba9
goulven <goulven.cadot@gmail.com>
2018-10-09 13:44:55 +0200

    suite du code de création

--- creation_compte.php
+++ creation_compte.php
@@ -2,23 +2,58 @@
 	session_start();
 
 	$host = "localhost";
-	$dbname = "Workshop";
+	$dbname = "workshop";
 	$id_db = "root";
 	$mdp_db = "";
 
 	try 
 	{
 	    // create connexion
-		$dbh = new PDO('mysql:host='+$host+';dbname='+dbname, "", "");
-		    
+		$dbh = new PDO('mysql:host='.$host.';dbname='.$dbname, $id_db, $mdp_db);    
 	}
-	 catch (PDOException $e) {
+
+	catch (PDOException $e) {
 	     print "Erreur !: " . $e->getMessage() . "<br/>";
 	     die();
 	}
 	
+	function verif()
+	{
+		if(!(isset($_POST["nom"])) || $_POST["nom"] == '')
+		{
+			return (false);
+		}
+		if(!(isset($_POST["prenom"])) || $_POST["prenom"] == '')
+		{
+			return (false);
+		}
+		if(!(isset($_POST["pseudo"])) || $_POST["pseudo"] == '')
+		{
+			return (false);
+		}
+		if(!(isset($_POST["mdp"])) || $_POST["mdp"] == '')
+		{
+			return (false);
+		}
+		if(!(isset($_POST["ville"])) || $_POST["ville"] == '')
+		{
+			return (false);
+		}
+		if(!(isset($_POST["cp"])) || $_POST["cp"] == '')
+		{
+			return (false);
+		}
+		return (true);
+	}
 ?>
+<?php
+
+	if (!verif())
+	{
+		header('location: inscription.php?error=1');
+		exit();
+	}
 
 
-
+?>ma bite
 <?php $dbh = null;//deco de la base de donnee ?>
--- inscription.php
+++ inscription.php
@@ -1,7 +1,43 @@
 <div>
 	<form method='POST' action='creation_compte.php'>
 		<div>
-			<input
+			nom*
+			<input type='text' name='nom'>
 		</div>
+		<div>
+			prenom*
+			<input type='text' name='prenom'>
+		</div>
+		<div>
+			nom d'utilisateur*
+			<input type='text' name='pseudo'>
+		</div>
+		<div>
+			mot de passe*
+			<input type='text' name='mdp'>
+		</div>
+		<div>
+			email*
+			<input type='text' name='mail'>
+		</div>
+		<div>
+			telephone*
+			<input type='text' name='telephone'>
+		</div>
+		<div>
+			code postale*
+			<input type='integer' name='cp'>
+		</div>
+		<div>
+			ville*
+			<input type='text' name='ville'>
+		</div>
+		<div>
+			sexe
+			<input type='radio' id='f' name='sexe' value='F'><label for='f'>F</label>
+			<input type='radio' id='m' name='sexe' value='M' checked><label for='m'>M</label>
+			<input type='radio' id='a' name='sexe' value='A'><label for='a'>Autre</label>
+		</div>
+		<button type='submit'>Valider</button>
 	</form>
 </div>
